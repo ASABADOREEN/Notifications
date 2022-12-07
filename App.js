74b0920cbecdd9b3a62a6ad048ea67839744a883
ASABA DOREEN <doreenasaba187@gmail.com>
2022-12-07 13:13:35 +0300

    Pushing notifications

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import registerNNPushToken from 'native-notify';
+
 
 export default function App() {
+  registerNNPushToken(5181, 'HiCoLVY9G6elbMM1MDRbxD');
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
